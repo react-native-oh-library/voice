d00c80eb60d730f1e8d44290e8a953f939a16edc
Inkmu <sunxiangen@h-partners.com>
2024-12-20 10:55:25 +0800

    chore: add license header and format code style

--- src/VoiceModuleTypes.ts
+++ src/VoiceModuleTypes.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
 import { EventSubscriptionVendor } from 'react-native';
 
 type Callback = (error: string) => void;
